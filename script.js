60d13bbcb0f0078d837656551023f594e9da3171
Maël Gruand <maelgruand1@yahoo.com>
2024-12-16 14:34:42 +0100

    modif

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-import { module } from "./module.js";
+const elements = {
+    section : document.getElementById("description"),
+    btnAccess : document.getElementById("access"),
+    hidden : "hidden",
+};
+function seeSection(){
+    if(elements.btnAccess.addEventListener("click")){
+        elements.section.classList.remove(elements.hidden);
+    }
+    else{
+        elements.section.classList.add(elements.hidden);
+    }
+}
+function hideSection(){
+    if(elements.btnAccess.addEventListener("click")){
+        elements.section.classList.add(elements.hidden);
+    }
+    else{
+        elements.section.classList.remove(elements.hidden)
+    }
+}
+function action(){
+    if (elements.btnAccess.addEventListener("click")){
+        seeSection();
+
+    }
+    else{
+        hideSection();
+    }
+}
