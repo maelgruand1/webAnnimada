cf0dbbdbcaf0be21ec1dd254bc635ad571c9af4f
Maël Gruand <maelgruand1@yahoo.com>
2024-12-16 14:28:39 +0100

    modif

--- module.js
+++ module.js
@@ -1,31 +1,34 @@
-const elements = {
-    section : document.getElementById("description"),
-    btnAccess : document.getElementById("access"),
-    hidden : "hidden",
-};
-function seeSection(){
-    if(elements.btnAccess.addEventListener("click")){
-        elements.section.classList.remove(elements.hidden);
-    }
-    else{
-        elements.section.classList.add(elements.hidden);
+ export class module {
+    constructor(parameters) {
+        const elements = {
+            section : document.getElementById("description"),
+            btnAccess : document.getElementById("access"),
+            hidden : "hidden",
+        };
+        function seeSection(){
+            if(elements.btnAccess.addEventListener("click")){
+                elements.section.classList.remove(elements.hidden);
+            }
+            else{
+                elements.section.classList.add(elements.hidden);
+            }
+        }
+        function hideSection(){
+            if(elements.btnAccess.addEventListener("click")){
+                elements.section.classList.add(elements.hidden);
+            }
+            else{
+                elements.section.classList.remove(elements.hidden)
+            }
+        }
+        function action(){
+            if (elements.btnAccess.addEventListener("click")){
+                seeSection();
+        
+            }
+            else{
+                hideSection();
+            }
+        }
     }
 }
-function hideSection(){
-    if(elements.btnAccess.addEventListener("click")){
-        elements.section.classList.add(elements.hidden);
-    }
-    else{
-        elements.section.classList.remove(elements.hidden)
-    }
-}
-function action(){
-    if (elements.btnAccess.addEventListener("click")){
-        seeSection();
-
-    }
-    else{
-        hideSection();
-    }
-}
-
